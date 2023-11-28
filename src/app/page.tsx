2c55f2733837a5d9b8750990a21a303477525b11
smarquezmgh <santiago.marquez@thedac.info>
2023-11-28 17:35:13 -0500

    comment removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <HomeUI/>
       <AvatarCarrouselUI />
       <ExploreNowUI />
-      {/* <CreationModeUI /> */}
-      {/* <VrmUI />
-      <InAppUI /> */}
+      <CreationModeUI />
+      <VrmUI />
+      <InAppUI />
     </main>
     <FooterUI/>
   </>
