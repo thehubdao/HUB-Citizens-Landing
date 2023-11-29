fa6113de938765fb18944a5364f83a8a3d20dd28
smarquezmgh <santiago.marquez@thedac.info>
2023-11-28 19:12:02 -0500

    updated component and styles

--- src/ui/creationMode/creationMode.ui.tsx
+++ src/ui/creationMode/creationMode.ui.tsx
@@ -1,21 +1,24 @@
-import { BorderColor, ButtonSize, TextColor } from "@/enum/common.enum";
 import IconButtonUI from "../common/iconButton.ui";
-import ExploreNowUI from "./exploreNow.ui";
+import { ButtonIcon } from "@/enum/ui.enum";
 
 export default function CreationModeUI() {
   return (
-    <div className="bg-ctz-gray-aultra_light ctz-padding relative">
-      <div className="h-full lg:min-h-screen w-full flex flex-col lg:flex-row pb-20 lg:pb-0">
-        <div className="w-full lg:w-[37%] h-full lg:h-[756px] border-[2px] border-ctz-gray-dark flex flex-col justify-between">
-          <div className="px-10 flex flex-col justify-center items-center lg:items-start flex-1 text-center lg:text-start pt-10 lg:pt-0">
-            <p className="text-ctz-pink-light text-[40px] leading-[80%]">002.</p>
-            <h1 className="text-ctz-gray-dark text-[8vw] lg:text-[4.1vw] leading-[80%] py-5">CREATION MODE</h1>
+    <div className="bg-ctz-gray-aultra_light ctz-padding relative pb-20 lg:pb-32">
+      <div className="flex flex-col lg:flex-row">
+        <div className="border-[2px] border-b-0 lg:border-r-0 border-ctz-gray-dark flex flex-col items-center">
+          <div className="px-10 text-center lg:text-start">
+            <p className="text-ctz-pink-light text-[40px] leading-[80%] pt-20 lg:pt-40">002.</p>
+            <h1 className="text-ctz-gray-dark text-[8vw] lg:text-[4.1vw] leading-[80%] py-4">CREATION MODE</h1>
             <h2 className="text-ctz-pink-light text-[40px] leading-[80%]">MAKE IT YOUR OWN!</h2>
-            <p className="font-work text-ctz-gray-semi_dark text-[3.5vw] lg:text-[1.3vw] tracking-[5px] leading-[100%] font-light py-10">Diversity at Your Fingertips: <br className="hidden lg:inline-block"/>Whether you want to upload a <br className="hidden lg:inline-block"/>single asset or a whole <br className="hidden lg:inline-block"/>collection, our platform adapts to your creative flow</p>
+            <p className="font-work text-ctz-gray-semi_dark text-[4vw] lg:text-[1.3vw] tracking-[5px] leading-[100%] font-light py-10">Diversity at Your Fingertips: <br className="hidden lg:inline-block" />Whether you want to upload a <br className="hidden lg:inline-block" />single asset or a whole <br className="hidden lg:inline-block" />collection, our platform adapts to your creative flow</p>
           </div>
-          <IconButtonUI arrow={'/icons/yellow-arrow.png'} children={'Explore it now!'} borderColor={BorderColor.Black} buttonSize={ButtonSize.Small} textColor={TextColor.orange} />
+          <div className="p-4 lg:p-0">
+            <IconButtonUI iconColor={'fill-ctz-yellow-medium'} icon={ButtonIcon.arrow}>
+              <p className='text-[4vw] lg:text-[2vw] leading-[80%] uppercase text-ctz-pink-light px-4 lg:px-12 text-center'>Upload your<br />own creations!</p>
+            </IconButtonUI>
+          </div>
         </div>
-        <div className="w-full lg:w-[63%] hpfull lg:h-[756px] border-y-[2px] border-r-[2px] border-ctz-gray-dark">
+        <div className="border-[2px] border-ctz-gray-dark w-full">
 
         </div>
       </div>
