f4f4dc146442542d2e201b7771a52545ef4fc562
smarquezmgh <santiago.marquez@thedac.info>
2023-11-29 20:04:04 -0500

    fixed footer styles

--- src/ui/footer/footer.ui.tsx
+++ src/ui/footer/footer.ui.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function FooterUI() {
   return (
-    <div className="font-work font-light text-lg text-ctz-gray-light leading-7 w-full  xl:h-[347px] flex flex-col xl:flex-row justify-between items-center bg-ctz-gray-semi_dark py-10">
-      <div className="w-full xl:w-[730px] flex flex-col items-center pb-10 xl:pb-0 mr-0 2xl:mr-[220px]">
+    <div className="font-work font-light text-lg text-ctz-gray-light leading-7 w-full flex flex-col xl:flex-row justify-between items-center bg-ctz-gray-semi_dark p-10">
+      <div className="py-10 lg:py-24 pl-0 lg:pl-20 2xl:pl-32 flex flex-col items-center">
         <p className="font-monument text-ctz-yellow-light text-[33px] leading-[80%]">
           THE HUB <br />CITIZENS
         </p>
@@ -12,31 +12,29 @@ export default function FooterUI() {
           All rights reserved 2023
         </p>
       </div>
-      <div className="w-full flex flex-wrap justify-around xl:items-center gap-y-4">
-        <div className='text-center xl:text-end'>
+      <div className="flex flex-wrap justify-around xl:items-center gap-y-4">
+        <div className='text-center xl:text-end flex flex-col gap-y-[6px]'>
           <Link href={CommonUrls.InvestorPrivacyNote}>
-            <p className='py-[3px]'>Terms & Conditions</p>
+            <p>Terms & Conditions</p>
           </Link>
           <Link href={CommonUrls.PrivacyPolicy}>
-            <p className='py-[3px]'>Privacy Policy</p>
+            <p>Privacy Policy</p>
           </Link>
           <Link href={CommonUrls.InvestorPrivacyNote}>
-            <p className='py-[3px]'>Investor Privacy Notice</p>
+            <p>Investor Privacy Notice</p>
           </Link>
           <Link href={CommonUrls.CookiesPolicy}>
-            <p className='py-[3px]'>Cookies Policy</p>
-          </Link> 
+            <p>Cookies Policy</p>
+          </Link>
         </div>
-        <div className='w-1/2 xl:w-[283px] text-center xl:text-start mx-6 xl:mx-[54px]'>
-          <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.
-          </p>
-        </div>
-        <div className='flex flex-col justify-center items-center'>
+        <p className='text-center xl:text-start pl-0 lg:pl-[54px] mr- lg:mr-28 2xl:mr-[280px]'>
+          Sed ut perspiciatis unde omnis <br className="hidden lg:inline-block" />iste natus error sit voluptatem <br className="hidden lg:inline-block" />accusantium doloremque <br className="hidden lg:inline-block" />laudantium, totam rem aperiam, <br className="hidden lg:inline-block" />eaque ipsa quae ab illo inventore <br className="hidden lg:inline-block" />veritatis.
+        </p>
+        <div className='flex flex-col justify-center items-center pr-0 lg:pr-14 2xl:pr-[110px]'>
           <p className='uppercase text-[17px] leading-5 -tracking-[0.672px]'>Contact us at</p>
           <Link href={ExternalLink.MailTo}>
             <p className='cursor-pointer hover:text-ctz-yellow-medium'>Citizens@thehubdao.xzy</p>
-          </Link> 
+          </Link>
         </div>
       </div>
     </div>
