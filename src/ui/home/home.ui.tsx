08014ffdff5ac77aa45d8e45b9ed78727260c22b
smarquezmgh <santiago.marquez@thedac.info>
2023-11-29 20:06:25 -0500

    fixed home styles

--- src/ui/home/home.ui.tsx
+++ src/ui/home/home.ui.tsx
@@ -27,27 +27,27 @@ export default function HomeUI() {
   return (
     <div className="bg-ctz-yellow-medium">
       {/* first section */}
-      <div className="pt-[156px] sm:pt-[73px] lg:min-h-screen">
+      <div className="pt-[73px] lg:min-h-screen flex flex-col justify-between pb-10">
         <div className="h-[73px] border-b-[2px] border-ctz-light flex justify-center items-center ctz-padding">
           <ColorsBarUI />
         </div>
-        <div className="text-center ctz-padding">
-          <h1 className="text-ctz-yellow-light text-[13vw] lg:text-[15vw] leading-[80%] pt-[51px]">THE HUB <br />CITIZENS</h1>
-          <p className="text-ctz-pink-medium text-[3.75vw] leading-[80%] tracking-[7.2px]">WEB 3.0 ORIENTED AVATAR SUITE</p>
+        <div className="text-center ctz-padding pb-20 pt-10 lg:pt-0">
+          <h1 className="text-ctz-yellow-light text-[12vw] sm:text-[13vw] lg:text-[14vw] 2xl:text-[15vw] leading-[80%]">THE HUB <br />CITIZENS</h1>
+          <p className="text-ctz-pink-medium text-[3.5vw] sm:text-[5vw] lg:text-[2.8vw] 2xl:text-[3.4vw] leading-[80%] tracking-[7.2px]">WEB 3.0 ORIENTED AVATAR SUITE</p>
         </div>
-        <div className='flex flex-col lg:flex-row justify-center items-center lg:justify-between pt-8 lg:pt-[76px] ctz-padding'>
+        <div className='flex flex-col lg:flex-row justify-center items-center lg:justify-between ctz-padding'>
           <IconButtonUI iconColor={'fill-ctz-gray-dark'} icon={ButtonIcon.play} iconSize={ButtonIconSize.small} borderColor={ButtonBorderColor.White}>
             <p className='text-ctz-light text-lg leading-[80%] px-6 text-center'>WATCH TRAILER</p>
           </IconButtonUI>
           <div className='my-6 lg:my-0'>
             <IconButtonUI iconColor={'fill-ctz-light'} iconsList={icons} iconSize={ButtonIconSize.small} borderColor={ButtonBorderColor.White}>
-              <p className='text-ctz-light text-lg leading-[80%] px-20 text-center'>THE HUB DAO</p>
+              <p className='text-ctz-light text-lg leading-[80%] px-5 lg:px-20 text-center'>THE HUB DAO</p>
             </IconButtonUI>
           </div>
         </div>
       </div>
       {/* second section */}
-      <div className="pb-20 lg:pb-40 ctz-padding">
+      <div className="pb-20 lg:pb-40 ctz-padding lg:pt-52">
         <CitizenPortalUI />
       </div>
     </div>
