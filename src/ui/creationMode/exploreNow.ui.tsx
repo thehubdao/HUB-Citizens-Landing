53183b5421c62a4b394daf3c0443c78dfeaa2bff
smarquezmgh <santiago.marquez@thedac.info>
2023-11-30 15:01:26 -0500

    adjusted font size

--- src/ui/creationMode/exploreNow.ui.tsx
+++ src/ui/creationMode/exploreNow.ui.tsx
@@ -5,7 +5,7 @@ import { ButtonIcon } from '@/enum/ui.enum';
 export default function ExploreNowUI() {
   return (
     <div className='w-full flex flex-col justify-center items-center gap-y-14 py-20 overflow-x-hidden ctz-padding bg-ctz-gray-aultra_light'>
-      <p className='font-work text-ctz-gray-dark text-[25px] leading-[100%] font-light text-center tracking-[5px]'>Your creative process deserves a <br className="hidden lg:inline-block"/> dedicated space. Our Creator HUB is the <br className="hidden lg:inline-block"/>epicenter of innovation, where you can <br className="hidden lg:inline-block"/>	design and modify avatars, creatures <br className="hidden lg:inline-block"/>and accessories in a seamless way. </p>
+      <p className='font-work text-ctz-gray-dark text-[3.5vw] lg:text-[1.3vw] leading-[100%] font-light text-center tracking-[5px]'>Your creative process deserves a <br className="hidden lg:inline-block"/> dedicated space. Our Creator HUB is the <br className="hidden lg:inline-block"/>epicenter of innovation, where you can <br className="hidden lg:inline-block"/>	design and modify avatars, creatures <br className="hidden lg:inline-block"/>and accessories in a seamless way. </p>
       <IconButtonUI iconColor={'fill-ctz-yellow-medium'} icon={ButtonIcon.arrow}>
         <p className='text-2xl lg:text-[2.5vw] leading-[80%] uppercase text-ctz-pink-light px-6 lg:px-12 text-center'>Explore it now!</p>
       </IconButtonUI>
