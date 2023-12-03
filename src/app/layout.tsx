2a6af4c46f9c012fcaf3f5a7fe5f48fd19f2f768
Andres <andgonzalezcas@unal.edu.co>
2023-12-02 21:17:55 -0500

    Update Main layout to get redux context and fetch data.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import { Work_Sans } from 'next/font/google';
 import Monument_Font from 'next/font/local';
 import ToogleIcons from 'next/font/local';
 import './globals.css';
+import { ProviderRedux } from '@/redux/ui/provider.redux';
+import GetDataRedux from '@/redux/ui/getData.redux';
 
-const workSans = Work_Sans({ subsets: ['latin'], display: 'block', variable: '--work-font'});
-const MonumentFont = Monument_Font({src: '../../public/fonts/MonumentExtended-Ultrabold.otf', display: 'block', variable: '--monument-font'});
-const toogleIcons = ToogleIcons({src: '../../public/fonts/icomoon-toogle-x.ttf', display: 'block',  variable: '--toogle-font'});
+const workSans = Work_Sans({ subsets: ['latin'], display: 'block', variable: '--work-font' });
+const MonumentFont = Monument_Font({ src: '../../public/fonts/MonumentExtended-Ultrabold.otf', display: 'block', variable: '--monument-font' });
+const toogleIcons = ToogleIcons({ src: '../../public/fonts/icomoon-toogle-x.ttf', display: 'block', variable: '--toogle-font' });
 
 export const metadata: Metadata = {
   title: 'The Hub Citizens',
@@ -20,9 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${workSans.variable} ${MonumentFont.className} ${MonumentFont.variable}  ${toogleIcons.variable} overflow-x-hidden`}>
-      <body>
-        {children}
-        </body>
+      <ProviderRedux>
+        <GetDataRedux>
+          <body>
+            {children}
+          </body>
+        </GetDataRedux>
+      </ProviderRedux>
     </html>
   )
 }
