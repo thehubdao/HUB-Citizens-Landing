2b86d9cb4e8904111783359bd8f15523a40afea3
GabCh15 <info@thedac.info>
2024-03-07 22:01:24 -0500

    change text

--- src/ui/vrm/vrm.ui.tsx
+++ src/ui/vrm/vrm.ui.tsx
@@ -39,7 +39,7 @@ export default function VrmUI() {
           <div className="text-center lg:text-end pt-20 lg:pt-40">
             <h1 className="text-ctz-gray-dark text-[10vw] lg:text-[6.61vw] leading-[80%]"><span className="text-ctz-pink-light">003.</span>VRM <br />INTEGRATION</h1>
             <h2 className="text-ctz-pink-light text-[6vw] lg:text-[2.87vw] leading-[80%] uppercase pt-5 lg:pt-0">Focus on Interoperability:</h2>
-            <p className="font-work text-ctz-gray-semi_dark text-[3.5vw] lg:text-[1.3vw] tracking-[5px] leading-[100%] font-light py-10">Our VRM integration <br className="hidden lg:inline-block" />allows your creations to <br className="hidden lg:inline-block" />be compatible with over <br className="hidden lg:inline-block" />200 platforms.</p>
+            <p className="font-work text-ctz-gray-semi_dark text-[3.5vw] lg:text-[1.3vw] tracking-[5px] leading-[100%] font-light py-10">Our VRM integration <br className="hidden lg:inline-block" />allows your creations to <br className="hidden lg:inline-block" />be compatible with over <br className="hidden lg:inline-block" />25 platforms.</p>
           </div>
           <IconButtonUI iconColor={'fill-ctz-pink-medium'} icon={ButtonIcon.arrow} borderColor={ButtonBorderColor.White} iconLink={PageLocation.Resources}>
             <p className='text-[4vw] lg:text-[2vw] leading-[80%] uppercase text-ctz-light px-4 lg:px-12 text-center'>Learn more</p>
